9285ff7099a1370b960f537f942295327b0e6f48
dileepchoudhary469 <dileepchoudhary469@gmail.com>
2024-05-09 12:34:27 +0000

    This is initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className=" text-3xl text-orange-400 text-center mt-56">This project is created using googl's idx</h1>
+    </main>
+  );
+}
